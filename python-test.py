f0d350e3d05c9e54cbb4e0c70c89098d90f0ca8f
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-25 17:36:47 -0400

    Update python-test.py: Add variable assignment and print statement

--- python-test.py
+++ python-test.py
@@ -3,3 +3,8 @@
 # ... ^^^ this reduces many complications that can arise from conflicting package versions.
 # use Venv or Anaconda with Python: Create Environment.
 # Note: By using the .py file extension, you tell VS Code to interpret this file as a Python program, so that it evaluates the contents with the Python extension and the selected interpreter.
+
+# msg is a variable that stores the value "Roll a dice!".
+# The (=) operator is used to assign the value on the right-hand side ("Roll a dice!") to the variable on the left-hand side (msg).
+msg = "Roll a dice!"
+print(msg)

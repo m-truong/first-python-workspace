85a93a126be3e8a2ae4e949f1917ee1f7569837f
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-25 17:34:35 -0400

    Update python-test.py: Add note on .py file extension for VS Code

--- python-test.py
+++ python-test.py
@@ -2,3 +2,4 @@
 # Once you activate that environment, any packages you then install are isolated from other environments, including the global interpreter environment.
 # ... ^^^ this reduces many complications that can arise from conflicting package versions.
 # use Venv or Anaconda with Python: Create Environment.
+# Note: By using the .py file extension, you tell VS Code to interpret this file as a Python program, so that it evaluates the contents with the Python extension and the selected interpreter.

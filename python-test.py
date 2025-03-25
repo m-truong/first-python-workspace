7c80674d2213dff4c2e2471c35b168d607dd33a6
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-25 17:30:03 -0400

    Initial commit: Add python-test.py

---
+++ python-test.py
@@ -0,0 +1,4 @@
+# A best practice among Python developers is to use a project-specific virtual environment.
+# Once you activate that environment, any packages you then install are isolated from other environments, including the global interpreter environment.
+# ... ^^^ this reduces many complications that can arise from conflicting package versions.
+# use Venv or Anaconda with Python: Create Environment.
